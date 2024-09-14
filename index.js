fbc9dc854b23244efb6c46389132a1d5b823b815
Joao19822 <jv261555@gmail.com>
2024-09-13 21:58:14 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+//novo usuario
+
+//representar o botão no js
+var btn_registrar = document.getElementById('btn_registrar')
+
+//clicar no botão registrar
+btn_registrar.addEventListener('click', (e)=>{
+    e.preventDefault();
+
+    //apresentar campos confime sua senha
+    document.getElementById('area_confirma_senha').classList.remove('d-none')
+
+    //mudar o texto do botão para cadastrar
+    document.getElementById('btn_registrar').classList.add('d-none')
+    document.getElementById('btn_cadastrar').classList.remove('d-none')
+})
+
+//lista_usuario
+let lista_usuario = [];
+
+var btn_cadastrar = document.getElementById('btn_cadastrar')
+
+btn_cadastrar.addEventListener('click', e=> {
+    e.preventDefault();
+    
+    if (document.getElementById('nome_usuario').value == "") {
+        document.getElementById('nome_usuario').classList.add('border')
+        document.getElementById('nome_usuario').classList.add('border-danger')
+        alert('preencha o nome')
+    } else {
+        //verificar se a senha e confirma senhao sao iguais
+        if (document.getElementById('senha_usuario').value != document.getElementById
+        ('confirma_senha').value) {
+            alert('ops, as senhas não batem')
+        }else{
+            //cadastrar ué
+            lista_usuario.push({
+                nome: document.getElementById('nome_usuario').value,
+                senha:document.getElementById('senha_usuario').value
+            })
+            localStorage.setItem('lista_usuario',JSON.stringify(lista_usuario))
+            recuperar_usuario()
+        }
+    }
+
+})
+
+function recuperar_usuario(){
+    lista_usuario = []
+    let conteudo_lista = JSON.parse(localStorage.getItem('lista_usuario'))
+    conteudo_lista.forEach(item => {
+        lista_usuario.push({
+            nome: item.nome,
+            senha:item.senha
+        })
+    });
+}
+
+recuperar_usuario()
